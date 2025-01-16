b39977772593b5018978680e51a1710876786b61
Munem-Prionto <munem.prionto@gmail.com>
2025-01-17 02:01:27 +0600

    basic starter code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,39 +1,33 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/main'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
+    path: '/main',
+    component: () => import('@/views/Main.vue') // Page for user input
+  },
+  {
+    path: '/choice',
+    component: () => import('@/views/ChoicePage.vue') // Page with Box A and Box B
+  },
+  {
+    path: '/boxA',
+    component: () => import('@/views/Child.vue') // Page for Box A
+  },
+  {
+    path: '/boxB',
+    component: () => import('@/views/Parent.vue') // Page for Box B
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
+
---
+++ src/views/Main.vue
@@ -0,0 +1,47 @@
+<template>
+    <ion-page>
+        <!-- <ion-header>
+            <ion-toolbar>
+                <ion-title class="text-center">Moner Bondhu</ion-title>
+            </ion-toolbar>
+        </ion-header> -->
+        <ion-content :fullscreen="true">
+            <section class=" flex w-full items-center justify-center h-full  p-3">
+                <div class="p-4 w-full">
+                    <img src="../resources/icon.png" alt="Moner Bondhu Logo" class="mb-8" />
+                    <h2 class="mb-4">Welcom to Manush<span class="text-red-500">E</span> </h2>
+                    <form action="" class="flex justify-between">
+                        <input type="text"
+                            class="shadow-xs flex w-full items-center justify-center rounded-lg border border-stroke bg-white p-2  text-lg font-medium text-gray-5 outline-none  " />
+
+                    </form>
+                    <ion-button expand="block" fill="outline" class="mt-4">
+                        LOGIN
+                    </ion-button>
+                </div>
+            </section>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonIcon } from '@ionic/vue';
+
+import { heart, logoApple, settingsSharp, star } from 'ionicons/icons';
+</script>
+<style scoped>
+* {
+    font-family: 'Poppins', sans-serif !important;
+}
+
+ion-button {
+    --background: #ce2327;
+
+
+    --color: white;
+
+
+    --border-width: 0px;
+
+}
+</style>
